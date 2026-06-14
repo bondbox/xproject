e8bb74710a545b03c22cc7387c60783408e88b46
Mingzhe Zou <zoumingzhe@qq.com>
2026-06-14 15:56:39 +0800

    xproject-python: remove xpip-build dependency

--- xproject-python/xproject_python/attribute.py
+++ xproject-python/xproject_python/attribute.py
@@ -1,8 +1,11 @@
 # coding:utf-8
 
-__version__ = "0.1"
-
 # project info
 __project_name__ = "xproject"
 __project_home__ = "https://github.com/bondbox/xproject/"
 __project_desc__ = "Initialize project files"
+
+# package info
+__version__ = "0.1"
+__package_name__ = "xproject-python"
+__package_desc__ = "Initialize project files"
